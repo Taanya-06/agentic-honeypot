5f904979364ba05b79e46ee0a2267054b30a0567
Taanya-06 <taanya609@gmail.com>
2026-02-03 01:09:49 +0530

    modify

--- agent.py
+++ agent.py
@@ -7,6 +7,10 @@ def agent_reply():
         "I already completed KYC, why again?",
         "Is there another way to fix this?",
         "I am not sure I understand, please help.",
+        "I’m not sure I understood this properly.",
+        "Okay, what should I do next?",
+        "Can you give me more details?",
+        "Please clarify what you mean.",
         "What details do you need from me?",
         "Is there any official notice for this?"
     ]
